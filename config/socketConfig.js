64fbb150baa0fc3b7a0870681a0cc4e96761fb5f
msquaretechn-tech <msquaretechn@gmail.com>
2026-07-28 13:01:32 +0530

    Fix socket CORS: wedlock.au -> wedlock.com.au

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -8,7 +8,7 @@ let io;
 export const intializeSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: ["https://wedlock.au", "https://admin.wedlock.au", 'http://localhost:5173', 'http://localhost:5176'],
+            origin: ["https://wedlock.com.au", "https://admin.wedlock.com.au", 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
             methods: ["GET", "POST"],
         },
     });
